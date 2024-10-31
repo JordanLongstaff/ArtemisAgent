f674ffb1d474266a54cfa068d1cbd38feb052cc4
Jordan Longstaff <jrdnlngstff@gmail.com>
2024-10-31 09:17:19 -0400

    Remove unnecessary WithDataTestName inheritance

--- IAN/util/src/test/kotlin/com/walkertribe/ian/util/SortingComparatorTest.kt
+++ IAN/util/src/test/kotlin/com/walkertribe/ian/util/SortingComparatorTest.kt
@@ -1,7 +1,6 @@
 package com.walkertribe.ian.util
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.datatest.WithDataTestName
 import io.kotest.datatest.withData
 import io.kotest.matchers.equals.shouldBeEqual
 
@@ -47,7 +46,9 @@ class SortingComparatorTest : DescribeSpec({
                             thirdComparator to sortingTestCase.sortByThird,
                         )
 
-                        val expectedSort = sortingTestCase.expectedSortIndices.map { unsortedTriples[it] }
+                        val expectedSort = sortingTestCase.expectedSortIndices.map {
+                            unsortedTriples[it]
+                        }
                         val actualSort = unsortedTriples.sortedWith(sorter)
                         actualSort shouldBeEqual expectedSort
                     }
@@ -62,7 +63,7 @@ enum class SortingTestCase(
     val sortBySecond: Boolean,
     val sortByThird: Boolean,
     vararg val expectedSortIndices: Int,
-) : WithDataTestName {
+) {
     FIRST_SECOND_THIRD(true, true, true, 1, 3, 7, 2, 5, 0, 4, 6),
     FIRST_SECOND(true, true, false, 1, 3, 2, 7, 0, 5, 4, 6),
     FIRST_THIRD(true, false, true, 1, 7, 2, 3, 4, 5, 0, 6),
@@ -70,12 +71,5 @@ enum class SortingTestCase(
     SECOND_THIRD(false, true, true, 1, 5, 0, 3, 4, 7, 2, 6),
     SECOND(false, true, false, 0, 1, 3, 5, 2, 4, 6, 7),
     THIRD(false, false, true, 1, 4, 5, 7, 0, 2, 3, 6),
-    NO_SORT(false, false, false, 0, 1, 2, 3, 4, 5, 6, 7);
-
-    override fun dataTestName(): String {
-        val sortFirst = if (sortByFirst) "S" else "Don't s"
-        val sortSecond = if (sortBySecond) "" else " don't"
-        val sortThird = if (sortByThird) "" else " don't"
-        return "${sortFirst}ort first,$sortSecond sort second,$sortThird sort third"
-    }
+    NO_SORT(false, false, false, 0, 1, 2, 3, 4, 5, 6, 7),
 }
