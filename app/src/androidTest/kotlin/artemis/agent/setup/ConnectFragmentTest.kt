74088112479eefa6ce807dfcf71a04517c35d6e9
Jordan Longstaff <jrdnlngstff@gmail.com>
2024-12-21 21:33:06 -0500

    More testing in ConnectFragmentTest

--- app/src/androidTest/kotlin/artemis/agent/setup/ConnectFragmentTest.kt
+++ app/src/androidTest/kotlin/artemis/agent/setup/ConnectFragmentTest.kt
@@ -11,12 +11,15 @@ import artemis.agent.R
 import artemis.agent.setup.settings.SettingsFragmentTest
 import com.adevinta.android.barista.assertion.BaristaEnabledAssertions.assertDisabled
 import com.adevinta.android.barista.assertion.BaristaEnabledAssertions.assertEnabled
+import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
 import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.clearText
 import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.writeTo
 import com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep
+import dev.tmapps.konnection.Konnection
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -39,16 +42,22 @@ class ConnectFragmentTest {
 
         assertEnabled(R.id.scanButton)
         assertNotDisplayed(R.id.scanSpinner)
+        assertDisplayed(R.id.noServersLabel, R.string.click_scan)
+        assertDisplayed(R.id.serverList)
+        assertRecyclerViewItemCount(R.id.serverList, 0)
 
         clickOn(R.id.scanButton)
 
         assertDisabled(R.id.scanButton)
         assertDisplayed(R.id.scanSpinner)
+        assertNotDisplayed(R.id.noServersLabel)
 
         sleep(scanTimeout.toLong(), TimeUnit.SECONDS)
 
         assertEnabled(R.id.scanButton)
         assertNotDisplayed(R.id.scanSpinner)
+        assertDisplayed(R.id.noServersLabel, R.string.no_servers_found)
+        assertRecyclerViewItemCount(R.id.serverList, 0)
     }
 
     @Test
@@ -64,11 +73,39 @@ class ConnectFragmentTest {
     }
 
     @Test
-    fun showNetworkInfoTest() {
+    fun connectionFailedTest() {
+        val connectTimeout = AtomicInteger()
+        activityScenarioRule.scenario.onActivity { activity ->
+            connectTimeout.lazySet(activity.viewModels<AgentViewModel>().value.connectTimeout)
+        }
+
+        assertDisplayed(R.id.connectLabel, R.string.not_connected)
+        assertNotDisplayed(R.id.connectSpinner)
+
+        writeTo(R.id.addressBar, "127.0.0.1")
+        clickOn(R.id.connectButton)
+
+        assertDisplayed(R.id.connectLabel, R.string.connecting)
+        assertDisplayed(R.id.connectSpinner)
+
+        sleep(connectTimeout.toLong(), TimeUnit.SECONDS)
+
+        assertDisplayed(R.id.connectLabel, R.string.failed_to_connect)
+        assertNotDisplayed(R.id.connectSpinner)
+
+        clickOn(R.id.connectButton)
+        assertDisplayed(R.id.connectLabel, R.string.failed_to_connect)
+        assertNotDisplayed(R.id.connectSpinner)
+    }
+
+    @Test
+    fun showNetworkInfoTest() = runTest {
         val showingInfo = AtomicBoolean()
         activityScenarioRule.scenario.onActivity { activity ->
             showingInfo.lazySet(activity.viewModels<AgentViewModel>().value.showingNetworkInfo)
         }
+
+        val hasConnection = Konnection.instance.getInfo()?.ipv4 != null
 
         val infoViews = intArrayOf(
             R.id.addressLabel,
@@ -86,8 +123,9 @@ class ConnectFragmentTest {
                 clickOn(R.id.connectPageButton)
             }
 
-            infoViews.forEach { resId ->
-                ArtemisAgentTestHelpers.assertDisplayed(resId, showing)
+            infoViews.forEachIndexed { viewIndex, resId ->
+                val couldBeNull = viewIndex == 0 && !hasConnection
+                ArtemisAgentTestHelpers.assertDisplayed(resId, showing && !couldBeNull)
             }
         }
     }
