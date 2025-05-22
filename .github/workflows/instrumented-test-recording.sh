ae2c08ef6167cc7a890bfd60d092d915d127d602
JordanLongstaff <jrdnlngstff@gmail.com>
2025-05-22 16:33:58 -0400

    Let screen record finish naturally with emulator

--- .github/workflows/instrumented-test-recording.sh
+++ .github/workflows/instrumented-test-recording.sh
@@ -6,7 +6,7 @@ set -x
 set +e
 echo "Starting instrumented tests..."
 ./gradlew connectedCheck &
-sleep 1
+sleep 10
 TEST_PID=$!
 echo "Starting the screen recording..."
 adb exec-out "while true; do screenrecord --bugreport --output-format=h264 -; done" | ffmpeg -i - testRecording-$API_LEVEL.mp4 &
@@ -14,9 +14,6 @@ sleep 1
 echo "Waiting for instrumented tests to finish..."
 wait $TEST_PID
 TEST_STATUS=$?
-echo "Stopping the screen recording..."
-kill $!
-echo "Test run completed with status $TEST_STATUS"
 # Wait for the screen recording process to exit
 sleep 1
 exit $TEST_STATUS
