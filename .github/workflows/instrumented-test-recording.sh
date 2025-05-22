36c1eec5061a684e53b2460b67bb9ebc62a0b7c1
JordanLongstaff <jrdnlngstff@gmail.com>
2025-05-22 11:26:49 -0400

    Return to screenrecord and use FFmpeg to combine recordings

--- .github/workflows/instrumented-test-recording.sh
+++ .github/workflows/instrumented-test-recording.sh
@@ -4,13 +4,13 @@ API_LEVEL=$1
 
 set -x
 echo "Starting the screen recording..."
-./scrcpy-*/scrcpy -r testRecording-$API_LEVEL.mp4 &
-echo $! > scrcpy_pid.txt
+adb shell "while true; do screenrecord --bugreport --output-format=h264 -; done" | ffmpeg -i - testRecording-$API_LEVEL.mp4 &
+echo $! > ffmpeg_pid.txt
 set +e
 ./gradlew connectedCheck
 TEST_STATUS=$?
 echo "Test run completed with status $TEST_STATUS"
-kill -2 $(cat scrcpy_pid.txt)
+kill -2 $(cat ffmpeg_pid.txt)
 # Wait for the screen recording process to exit
 sleep 1
 exit $TEST_STATUS
