32e152848a44957c335ff1e79eeaed05f6b695e8
JordanLongstaff <jrdnlngstff@gmail.com>
2025-05-22 15:59:25 -0400

    Add sleeps

--- .github/workflows/instrumented-test-recording.sh
+++ .github/workflows/instrumented-test-recording.sh
@@ -5,11 +5,14 @@ API_LEVEL=$1
 set -x
 set +e
 echo "Starting instrumented tests..."
-./gradlew connectedCheck & echo $! > test_pid.txt
+./gradlew connectedCheck &
+sleep 1
+TEST_PID=$!
 echo "Starting the screen recording..."
 adb exec-out "while true; do screenrecord --bugreport --output-format=h264 -; done" | ffmpeg -i - testRecording-$API_LEVEL.mp4 &
+sleep 1
 echo "Waiting for instrumented tests to finish..."
-cat test_pid.txt | xargs wait
+wait $TEST_PID
 TEST_STATUS=$?
 echo "Stopping the screen recording..."
 kill $!
