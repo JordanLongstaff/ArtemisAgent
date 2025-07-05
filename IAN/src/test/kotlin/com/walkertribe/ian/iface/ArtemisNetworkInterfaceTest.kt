a1d139ce1c5cb6f807ce6718a24be3f635a1f885
JordanLongstaff <jrdnlngstff@gmail.com>
2025-07-05 15:24:30 -0400

    Attempt again to remove arbitrary iteration count

--- IAN/src/test/kotlin/com/walkertribe/ian/iface/ArtemisNetworkInterfaceTest.kt
+++ IAN/src/test/kotlin/com/walkertribe/ian/iface/ArtemisNetworkInterfaceTest.kt
@@ -599,7 +599,7 @@ class ArtemisNetworkInterfaceTest :
                             val versionFixture = VersionPacketFixture(versionArb)
                             val disconnectEvents = mutableListOf<ConnectionEvent.Disconnect>()
 
-                            versionFixture.generator.checkAll(200) { data ->
+                            versionFixture.generator.checkAll { data ->
                                 versions.add(data.packetVersion)
 
                                 val connectDeferred =
