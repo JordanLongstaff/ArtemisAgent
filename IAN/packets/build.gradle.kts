895dd177b7cd2691bedfc0570717c0f0e6515a71
JordanLongstaff <jrdnlngstff@gmail.com>
2025-10-07 06:22:41 -0400

    Add more fixture dependencies to packets module

--- IAN/packets/build.gradle.kts
+++ IAN/packets/build.gradle.kts
@@ -59,7 +59,9 @@ dependencies {
 
     pitest(libs.bundles.arcmutate)
 
+    pitest(testFixtures(projects.ian.enums))
     pitest(testFixtures(projects.ian.listener))
+    pitest(testFixtures(projects.ian.util))
     pitest(testFixtures(projects.ian.vesseldata))
     pitest(testFixtures(projects.ian.world))
 }
