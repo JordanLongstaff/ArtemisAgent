0d75e2e294d2469a62ab92a676720a2df5f0955b
JordanLongstaff <jrdnlngstff@gmail.com>
2025-12-26 21:59:37 -0500

    Add Konsist tests

---
+++ app/konsist/src/test/kotlin/artemis/agent/game/status/StatusInfoKonsistTest.kt
@@ -0,0 +1,16 @@
+package artemis.agent.game.status
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withName
+import com.lemonappdev.konsist.api.verify.assertFalse
+import io.kotest.core.spec.style.DescribeSpec
+
+class StatusInfoKonsistTest :
+    DescribeSpec({
+        val statusPackage = Konsist.scopeFromPackage("artemis.agent.game.status", "app")
+        val shieldClasses = statusPackage.classes().withName("Shield")
+
+        it("StatusInfo.Shield cannot be a data class") {
+            shieldClasses.assertFalse { cls -> cls.hasDataModifier }
+        }
+    })
